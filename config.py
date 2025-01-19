012cdfccd6c09966d3f248f58ae84a84bffcbb4e
rotate-2018 <58215040+rotate-2018@users.noreply.github.com>
2025-01-19 21:08:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,87 @@
-accountId = 'accountID'
+accountId = '513924220'
 
 cookies = {
-    'JSESSIONID': 'ajax:value'
+    'JSESSIONID': 'ajax:6263875539178987497',
+    'lang': 'v=2&lang=en-us',
+    'bcookie': 'v=2&fa6d2bb2-395a-476e-88f6-909b6368e19c',
+    'bscookie': 'v=1&20250109115405d1e05fa1-332c-4cf3-80be-542a362f6032AQFqi9nB8BgvmaMCMdq4LvAjMRMOHr9E',
+    'lidc': 'b=TGST09:s=T:r=T:a=T:p=T:g=2936:u=1:x=1:i=1736424982:t=1736511382:v=2:sig=AQGctTV8rfTYXizX4Y7rtMJODh48xdDQ',
+    'li_rm': 'AQEYm1DlMWouwQAAAZRK6iwpRuUx9SX5kmFwc2fguGuYfjuNW3A9KLIn5gN7CKSX_GVPCQFBfSXBnMcp5dcAcx4AXsXH5nDaTo1G7GAUfjb_oCZeIjLaxESA',
+    'AMCV_14215E3D5995C57C0A495C55^%^40AdobeOrg': '-637568504^%^7CMCIDTS^%^7C20098^%^7CMCMID^%^7C86494866504400065712772222012752305379^%^7CMCAAMLH-1737029754^%^7C3^%^7CMCAAMB-1737029754^%^7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y^%^7CMCOPTOUT-1736432154s^%^7CNONE^%^7CvVersion^%^7C5.1.1^%^7CMCCIDH^%^7C-1338333190',
+    'AMCVS_14215E3D5995C57C0A495C55^%^40AdobeOrg': '1',
+    'aam_uuid': '87026537265128592322789916228224577320',
+    'gpv_pn': 'www.linkedin.com^%^2Fcampaignmanager^%^2Faccounts^%^2Fid-redacted^%^2Fcampaigns^%^2Fnew^%^2Fdetails',
+    's_plt': '1.12',
+    's_pltp': 'www.linkedin.com^%^2Fcampaignmanager^%^2Faccounts',
+    's_ppv': 'www.linkedin.com^%^2Fcampaignmanager^%^2Faccounts^%^2Fid-redacted^%^2Fcampaigns^%^2Fnew^%^2Fdetails^%^2C49^%^2C15^%^2C2507^%^2C4^%^2C10',
+    's_ips': '746',
+    's_tp': '5099',
+    's_tslv': '1736424981337',
+    's_cc': 'true',
+    'li_sugr': '4dfb77a7-8e34-461f-9ad2-953e26c3613a',
+    'UserMatchHistory': 'AQIi_tpnXmLVAAAAAZRK_gg-eHcR1l5d2q7nqu0LVC0yN0zykYiOX_cRRoUxyYWaTIUU1JO22nNVLFbYwvr6WafhxoEl1T9oF5cRARGwdEOjuKik8lHmSlJHq8RedkWgmVQJbw6qw8msGVxLe61U0zscdEwOWppEwXZCp3Nsiyf_m4LjSc-VRMWz11EUK5oa1yaXH40pdd_gwjOCy4FvIXTqAe9tqHOAnHsalMrisXpPz9SE1mQ7JPJZZFDrjnkt0E46Zi5zF1Hv5CFXG8CyBK9dp3qZ6QFBGq-Ez3Gt_vUaEDLjayVYD9TIJoV0p9oVbvAssiTmxx79xepUlqjrwjhnjpuKfWUrLXXSA6XddyaLLAOMvw',
+    'AnalyticsSyncHistory': 'AQJy3uvjohYm-AAAAZRK_gg-CzjpkyIMH81tLFU9XwC1hPZx-HQ_1vs88KVlYw7UADfCWXAV8eFQU6Oha6Kx5w',
+    'g_state': '{i_l:0}',
+    'li_at': 'AQEDAVILa0ADG99zAAABlEr4gCcAAAGUbwUEJ00Ap_EUVr83Xztk8l4I80bwxoNa1734hfaQrO_O3XRKW2Lu80ygZEUcI7mHrgeSxxadI02LHqApf2s-qk_givwEOTSb0-FP67N1c8Bjy0KQkj7zOgN5',
+    'liap': 'true',
+    's_sq': '^%^5B^%^5BB^%^5D^%^5D',
+    'timezone': 'Asia/Shanghai',
+    'li_theme': 'light',
+    'li_theme_set': 'app',
+    '_guid': 'ad9a5977-b089-4afc-bd00-9ea72a3deea7',
+    'fptctx2': 'taBcrIH61PuCVH7eNCyH0CYjjbqLuI8XF8pleSQW5Na^%^252b4ka02KTapeeRnDgpE3^%^252fjZ1smPJIz44uKX^%^252bqNri3MRBFu9HHWStW5YEqpfV6ZPcn1JubWMimPGa6gQgtEqey8C2dRd3GcJUDTRLbZidjGf4u0mVHxSzzEP^%^252fCH9hZ8^%^252bMFl7FHDFnsUcUrYw8So9Y3R1W2pYxbgBFozOjBMeMFdiLkjg^%^252fAmBdlUsBAbddsO6ILHxirZtY^%^252fac6kQ5O3TBcyd5UH1RbJEUEM2BBIZoXy1hcTWoWyw^%^252fOkGd35v6LN4D6nt54mNmN5cbMv4ycKY^%^252fkMfM0sMl2lZas03RG5U1tF9R9TWfzKB1dWb9pGuNZe5Udw^%^253d',
+    'dfpfpt': '8c4a437d1f6e4e1c81c97743a64d2718',
+    'lms_ads': 'AQHGLGu8PDIZ0wAAAZRK_gmv2dRJYEWTI3y0pm0D1BAbKcV7whpqZZV-AoTkujgo4sEe8We7am2xg4g7jPSlPItgKWMgBOmP',
+    'lms_analytics': 'AQHGLGu8PDIZ0wAAAZRK_gmv2dRJYEWTI3y0pm0D1BAbKcV7whpqZZV-AoTkujgo4sEe8We7am2xg4g7jPSlPItgKWMgBOmP',
+    '_uetsid': '7055e210ce8011ef8cf4e79d84dfbba9',
+    '_uetvid': '7055ff50ce8011ef8998d54d19aec503',
 }
 
 headers = {
-    'User-Agent': 'value'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0',
+    'Accept': '*/*',
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'X-RestLi-Protocol-Version': '2.0.0',
+    'X-LI-Lang': 'en_US',
+    'X-LI-Track': '{clientVersion:2.32.189,mpVersion:2.32.189,osName:web,timezoneOffset:8,timezone:Asia/Shanghai,deviceFormFactor:DESKTOP,mpName:campaign-manager-web,displayDensity:1.7647058823529411,displayWidth:2560.5882352941176,displayHeight:1600.5882352941176}',
+    'X-li-page-instance': 'urn:li:page:d_campaign_details;rXALEzb4SUOMWsPDUzPd9A==',
+    'Csrf-Token': 'ajax:6263875539178987497',
+    'X-LI-ER-KEY': 'urn:li:sponsoredAccount:513924220',
+    'X-HTTP-Method-Override': 'GET',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'https://www.linkedin.com',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.linkedin.com/campaignmanager/accounts/513924220/campaigns/new/details?campaignGroupId=718540564',
+    'Cookie': 'JSESSIONID=ajax:6263875539178987497; lang=v=2&lang=en-us; bcookie=v=2&fa6d2bb2-395a-476e-88f6-909b6368e19c; bscookie=v=1&20250109115405d1e05fa1-332c-4cf3-80be-542a362f6032AQFqi9nB8BgvmaMCMdq4LvAjMRMOHr9E; lidc=b=TGST09:s=T:r=T:a=T:p=T:g=2936:u=1:x=1:i=1736424982:t=1736511382:v=2:sig=AQGctTV8rfTYXizX4Y7rtMJODh48xdDQ; li_rm=AQEYm1DlMWouwQAAAZRK6iwpRuUx9SX5kmFwc2fguGuYfjuNW3A9KLIn5gN7CKSX_GVPCQFBfSXBnMcp5dcAcx4AXsXH5nDaTo1G7GAUfjb_oCZeIjLaxESA; AMCV_14215E3D5995C57C0A495C55^%^40AdobeOrg=-637568504^%^7CMCIDTS^%^7C20098^%^7CMCMID^%^7C86494866504400065712772222012752305379^%^7CMCAAMLH-1737029754^%^7C3^%^7CMCAAMB-1737029754^%^7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y^%^7CMCOPTOUT-1736432154s^%^7CNONE^%^7CvVersion^%^7C5.1.1^%^7CMCCIDH^%^7C-1338333190; AMCVS_14215E3D5995C57C0A495C55^%^40AdobeOrg=1; aam_uuid=87026537265128592322789916228224577320; gpv_pn=www.linkedin.com^%^2Fcampaignmanager^%^2Faccounts^%^2Fid-redacted^%^2Fcampaigns^%^2Fnew^%^2Fdetails; s_plt=1.12; s_pltp=www.linkedin.com^%^2Fcampaignmanager^%^2Faccounts; s_ppv=www.linkedin.com^%^2Fcampaignmanager^%^2Faccounts^%^2Fid-redacted^%^2Fcampaigns^%^2Fnew^%^2Fdetails^%^2C49^%^2C15^%^2C2507^%^2C4^%^2C10; s_ips=746; s_tp=5099; s_tslv=1736424981337; s_cc=true; li_sugr=4dfb77a7-8e34-461f-9ad2-953e26c3613a; UserMatchHistory=AQIi_tpnXmLVAAAAAZRK_gg-eHcR1l5d2q7nqu0LVC0yN0zykYiOX_cRRoUxyYWaTIUU1JO22nNVLFbYwvr6WafhxoEl1T9oF5cRARGwdEOjuKik8lHmSlJHq8RedkWgmVQJbw6qw8msGVxLe61U0zscdEwOWppEwXZCp3Nsiyf_m4LjSc-VRMWz11EUK5oa1yaXH40pdd_gwjOCy4FvIXTqAe9tqHOAnHsalMrisXpPz9SE1mQ7JPJZZFDrjnkt0E46Zi5zF1Hv5CFXG8CyBK9dp3qZ6QFBGq-Ez3Gt_vUaEDLjayVYD9TIJoV0p9oVbvAssiTmxx79xepUlqjrwjhnjpuKfWUrLXXSA6XddyaLLAOMvw; AnalyticsSyncHistory=AQJy3uvjohYm-AAAAZRK_gg-CzjpkyIMH81tLFU9XwC1hPZx-HQ_1vs88KVlYw7UADfCWXAV8eFQU6Oha6Kx5w; g_state={i_l:0}; li_at=AQEDAVILa0ADG99zAAABlEr4gCcAAAGUbwUEJ00Ap_EUVr83Xztk8l4I80bwxoNa1734hfaQrO_O3XRKW2Lu80ygZEUcI7mHrgeSxxadI02LHqApf2s-qk_givwEOTSb0-FP67N1c8Bjy0KQkj7zOgN5; liap=true; s_sq=^%^5B^%^5BB^%^5D^%^5D; timezone=Asia/Shanghai; li_theme=light; li_theme_set=app; _guid=ad9a5977-b089-4afc-bd00-9ea72a3deea7; fptctx2=taBcrIH61PuCVH7eNCyH0CYjjbqLuI8XF8pleSQW5Na^%^252b4ka02KTapeeRnDgpE3^%^252fjZ1smPJIz44uKX^%^252bqNri3MRBFu9HHWStW5YEqpfV6ZPcn1JubWMimPGa6gQgtEqey8C2dRd3GcJUDTRLbZidjGf4u0mVHxSzzEP^%^252fCH9hZ8^%^252bMFl7FHDFnsUcUrYw8So9Y3R1W2pYxbgBFozOjBMeMFdiLkjg^%^252fAmBdlUsBAbddsO6ILHxirZtY^%^252fac6kQ5O3TBcyd5UH1RbJEUEM2BBIZoXy1hcTWoWyw^%^252fOkGd35v6LN4D6nt54mNmN5cbMv4ycKY^%^252fkMfM0sMl2lZas03RG5U1tF9R9TWfzKB1dWb9pGuNZe5Udw^%^253d; dfpfpt=8c4a437d1f6e4e1c81c97743a64d2718; lms_ads=AQHGLGu8PDIZ0wAAAZRK_gmv2dRJYEWTI3y0pm0D1BAbKcV7whpqZZV-AoTkujgo4sEe8We7am2xg4g7jPSlPItgKWMgBOmP; lms_analytics=AQHGLGu8PDIZ0wAAAZRK_gmv2dRJYEWTI3y0pm0D1BAbKcV7whpqZZV-AoTkujgo4sEe8We7am2xg4g7jPSlPItgKWMgBOmP; _uetsid=7055e210ce8011ef8cf4e79d84dfbba9; _uetvid=7055ff50ce8011ef8998d54d19aec503',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'TE': 'trailers',
 }
 
 
-testheaders="""POST /campaign-manager-api/campaignManagerAudienceCounts HTTP/1.1
+testheaders = """POST /campaign-manager-api/campaignManagerAudienceCounts HTTP/2
 Host: www.linkedin.com
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0
+Accept: */*
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Accept-Encoding: gzip, deflate, br
+X-RestLi-Protocol-Version: 2.0.0
+X-LI-Lang: en_US
+X-LI-Track: {"clientVersion":"2.32.189","mpVersion":"2.32.189","osName":"web","timezoneOffset":8,"timezone":"Asia/Shanghai","deviceFormFactor":"DESKTOP","mpName":"campaign-manager-web","displayDensity":1.7647058823529411,"displayWidth":2560.5882352941176,"displayHeight":1600.5882352941176}
+X-li-page-instance: urn:li:page:d_campaign_details;rXALEzb4SUOMWsPDUzPd9A==
+Csrf-Token: ajax:6263875539178987497
+X-LI-ER-KEY: urn:li:sponsoredAccount:513924220
+X-HTTP-Method-Override: GET
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 225
+Origin: https://www.linkedin.com
+Connection: keep-alive
+Referer: https://www.linkedin.com/campaignmanager/accounts/513924220/campaigns/new/details?campaignGroupId=718540564
+Cookie: JSESSIONID="ajax:6263875539178987497"; lang=v=2&lang=en-us; bcookie="v=2&fa6d2bb2-395a-476e-88f6-909b6368e19c"; bscookie="v=1&20250109115405d1e05fa1-332c-4cf3-80be-542a362f6032AQFqi9nB8BgvmaMCMdq4LvAjMRMOHr9E"; lidc="b=TGST09:s=T:r=T:a=T:p=T:g=2936:u=1:x=1:i=1736424982:t=1736511382:v=2:sig=AQGctTV8rfTYXizX4Y7rtMJODh48xdDQ"; li_rm=AQEYm1DlMWouwQAAAZRK6iwpRuUx9SX5kmFwc2fguGuYfjuNW3A9KLIn5gN7CKSX_GVPCQFBfSXBnMcp5dcAcx4AXsXH5nDaTo1G7GAUfjb_oCZeIjLaxESA; AMCV_14215E3D5995C57C0A495C55%40AdobeOrg=-637568504%7CMCIDTS%7C20098%7CMCMID%7C86494866504400065712772222012752305379%7CMCAAMLH-1737029754%7C3%7CMCAAMB-1737029754%7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y%7CMCOPTOUT-1736432154s%7CNONE%7CvVersion%7C5.1.1%7CMCCIDH%7C-1338333190; AMCVS_14215E3D5995C57C0A495C55%40AdobeOrg=1; aam_uuid=87026537265128592322789916228224577320; gpv_pn=www.linkedin.com%2Fcampaignmanager%2Faccounts%2Fid-redacted%2Fcampaigns%2Fnew%2Fdetails; s_plt=1.12; s_pltp=www.linkedin.com%2Fcampaignmanager%2Faccounts; s_ppv=www.linkedin.com%2Fcampaignmanager%2Faccounts%2Fid-redacted%2Fcampaigns%2Fnew%2Fdetails%2C49%2C15%2C2507%2C4%2C10; s_ips=746; s_tp=5099; s_tslv=1736424981337; s_cc=true; li_sugr=4dfb77a7-8e34-461f-9ad2-953e26c3613a; UserMatchHistory=AQIi_tpnXmLVAAAAAZRK_gg-eHcR1l5d2q7nqu0LVC0yN0zykYiOX_cRRoUxyYWaTIUU1JO22nNVLFbYwvr6WafhxoEl1T9oF5cRARGwdEOjuKik8lHmSlJHq8RedkWgmVQJbw6qw8msGVxLe61U0zscdEwOWppEwXZCp3Nsiyf_m4LjSc-VRMWz11EUK5oa1yaXH40pdd_gwjOCy4FvIXTqAe9tqHOAnHsalMrisXpPz9SE1mQ7JPJZZFDrjnkt0E46Zi5zF1Hv5CFXG8CyBK9dp3qZ6QFBGq-Ez3Gt_vUaEDLjayVYD9TIJoV0p9oVbvAssiTmxx79xepUlqjrwjhnjpuKfWUrLXXSA6XddyaLLAOMvw; AnalyticsSyncHistory=AQJy3uvjohYm-AAAAZRK_gg-CzjpkyIMH81tLFU9XwC1hPZx-HQ_1vs88KVlYw7UADfCWXAV8eFQU6Oha6Kx5w; g_state={"i_l":0}; li_at=AQEDAVILa0ADG99zAAABlEr4gCcAAAGUbwUEJ00Ap_EUVr83Xztk8l4I80bwxoNa1734hfaQrO_O3XRKW2Lu80ygZEUcI7mHrgeSxxadI02LHqApf2s-qk_givwEOTSb0-FP67N1c8Bjy0KQkj7zOgN5; liap=true; s_sq=%5B%5BB%5D%5D; timezone=Asia/Shanghai; li_theme=light; li_theme_set=app; _guid=ad9a5977-b089-4afc-bd00-9ea72a3deea7; fptctx2=taBcrIH61PuCVH7eNCyH0CYjjbqLuI8XF8pleSQW5Na%252b4ka02KTapeeRnDgpE3%252fjZ1smPJIz44uKX%252bqNri3MRBFu9HHWStW5YEqpfV6ZPcn1JubWMimPGa6gQgtEqey8C2dRd3GcJUDTRLbZidjGf4u0mVHxSzzEP%252fCH9hZ8%252bMFl7FHDFnsUcUrYw8So9Y3R1W2pYxbgBFozOjBMeMFdiLkjg%252fAmBdlUsBAbddsO6ILHxirZtY%252fac6kQ5O3TBcyd5UH1RbJEUEM2BBIZoXy1hcTWoWyw%252fOkGd35v6LN4D6nt54mNmN5cbMv4ycKY%252fkMfM0sMl2lZas03RG5U1tF9R9TWfzKB1dWb9pGuNZe5Udw%253d; dfpfpt=8c4a437d1f6e4e1c81c97743a64d2718; lms_ads=AQHGLGu8PDIZ0wAAAZRK_gmv2dRJYEWTI3y0pm0D1BAbKcV7whpqZZV-AoTkujgo4sEe8We7am2xg4g7jPSlPItgKWMgBOmP; lms_analytics=AQHGLGu8PDIZ0wAAAZRK_gmv2dRJYEWTI3y0pm0D1BAbKcV7whpqZZV-AoTkujgo4sEe8We7am2xg4g7jPSlPItgKWMgBOmP; _uetsid=7055e210ce8011ef8cf4e79d84dfbba9; _uetvid=7055ff50ce8011ef8998d54d19aec503
+Sec-Fetch-Dest: empty
+Sec-Fetch-Mode: cors
+Sec-Fetch-Site: same-origin
+TE: trailers
 """
